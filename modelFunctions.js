c24e3f27483685b747ced04800eeca9cd285fb7a
Farh Kadry <23-101289@students.eui.edu.eg>
2025-12-24 23:45:58 +0200

    engine section setup

--- modelFunctions.js
+++ modelFunctions.js
@@ -1,30 +1,26 @@
-const model = document.getElementById("carModel");
-const viewBtns = document.querySelectorAll(".viewBtn");
-const arBtn = document.getElementById("arBtn");
+let model = document.getElementById("carModel");
+let viewBtns = document.querySelectorAll(".viewBtn");
+let arBtn = document.getElementById("arBtn");
 
-/* CAMERA PRESETS */
-const views = {
-  top:   "-90deg 90deg 0.12m",
-  front: "0deg 75deg 0.18m",
-  back:  "180deg 75deg 0.18m",
-  side:  "90deg 75deg 0.18m"
+// CAMERA 
+let views = {
+  top:   "90deg -10deg 0.1m",
+  front: "0deg 90deg 0.1m",
+  back:  "180deg 90deg 5m",
+  side:  "90deg 75deg 0.1m"
 };
-
-/* CHANGE VIEW */
 function changeView(viewName, clickedBtn) {
   model.cameraOrbit = views[viewName];
-
-  // active state handling
   viewBtns.forEach(btn => btn.classList.remove("active"));
   clickedBtn.classList.add("active");
 }
-
-/* BUTTON EVENTS */
 viewBtns.forEach(btn => {
   btn.addEventListener("click", () => {
     changeView(btn.dataset.view, btn);
   });
 });
+
+// ARBUTTON
 arBtn.addEventListener("click", () => {
   if (model.canActivateAR) {
     model.activateAR();
